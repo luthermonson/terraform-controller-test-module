1f4fcb47fd786833bb9f81c583568b9ea9154d33
Luther Monson <luther.monson@gmail.com>
2019-07-10 13:31:35 -0700

    trying hard coded k3s endpoint

--- main.tf
+++ main.tf
@@ -1,9 +1,24 @@
 variable "test_config_map" {}
 variable "test_secret" {}
+variable host {
+  default = "https://10.43.0.1"
+}
+
+resource "kubernetes_config_map" "test-config-map" {
+  metadata {
+    name      = "test-config-map"
+    namespace = "terraform-controller"
+  }
+
+  data = {
+    test_config_map = "${var.test_config_map}"
+    test_secret     = "${var.test_secret}"
+  }
+}
 
 output "test_config_map" {
-  value = "${var.test_config_map}"
+  value = "${kubernetes_config_map.test-config-map.data.test_config_map}"
 }
 output "test_secret" {
-  value = "${var.test_secret}"
+  value = "${kubernetes_config_map.test-config-map.data.test_secret}"
 }
