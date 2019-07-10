a3940b8e3ceed5fccfa64f2352ea04b2c94de2aa
Luther Monson <luther.monson@gmail.com>
2019-07-10 11:03:19 -0700

    changing var nams, new provider config

--- main.tf
+++ main.tf
@@ -1,16 +1,26 @@
-provider "kubernetes" {}
+variable "test_config_map" {}
+variable "test_secret" {}
+variable host {
+  default = "https://localhost:6443"
+}
+provider "kubernetes" {
+  host                   = "${var.host}"
+  token                  = "${file("/var/run/secrets/kubernetes.io/serviceaccount/token")}"
+  cluster_ca_certificate = "${file("/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")}"
+}
 
 resource "kubernetes_config_map" "test-config-map" {
   metadata {
-    name = "test-config-map"
+    name      = "test-config-map"
     namespace = "terraform-controller"
   }
 
   data = {
-    hostname = "cattle.io"
+    test_config_map = "${var.test_config_map}"
+    test_secret     = "${var.test_secret}"
   }
 }
 
 output "hostname" {
-    value = "${kubernetes_config_map.test-config-map.data.hostname}"
+  value = "${kubernetes_config_map.test-config-map.data.hostname}"
 }
