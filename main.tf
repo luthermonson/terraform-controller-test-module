7080b40f09cb291bedff9bf7a4a62b8229deeda0
Luther Monson <luther.monson@gmail.com>
2019-07-10 13:38:06 -0700

    k8s backend reconfigured

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 variable "test_config_map" {}
 variable "test_secret" {}
-variable host {
-  default = "https://10.43.0.1"
-}
 
+provider "kubernetes" {
+  host                   = "https://10.43.0.1"
+  token                  = "${file("/var/run/secrets/kubernetes.io/serviceaccount/token")}"
+}
 resource "kubernetes_config_map" "test-config-map" {
   metadata {
     name      = "test-config-map"
